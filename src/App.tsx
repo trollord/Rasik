375da84a3e41ef538d80f1cb621901016c43ece4
akshataparte <142744300+akshataparte@users.noreply.github.com>
2024-06-08 23:00:26 +0530

    our sevices, our work model

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,18 @@ import { ThemeProvider } from '@mui/material';
 import { HomePage } from './modules/homePage';
 import { theme } from './theme';
 import { ServicePage } from './modules/servicePage';
-import { About } from './modules/homePage/components/aboutUs';
 import "./global.css";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <Router>
+      <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           {/* The services page will open with a single service displayed. We will determine the method by which the service is selected and displayed on the page*/}
           <Route path="/services" element={<ServicePage />} />
-         
+
         </Routes>
-    </Router>
+      </Router>
     </ThemeProvider>
   );
 }
