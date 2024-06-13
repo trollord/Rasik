073e75bc3fe75f52550b28f7e6799c70eef057ac
akshayp <aparte@clarapath.com>
2024-06-13 13:19:11 -0400

    test map component added

--- src/modules/homePage/components/meetUs/index.tsx
+++ src/modules/homePage/components/meetUs/index.tsx
@@ -25,14 +25,17 @@ function MeetUs() {
         <Grid container item md={6} height={isMobile ? '25vh' : '40vh'} width="55vw" borderRadius="10px" overflow="hidden">
 
 
-          <GoogleMapReact
+          {/* <GoogleMapReact
 
             bootstrapURLKeys={{ key: "" }}
             defaultCenter={defaultProps.center}
             defaultZoom={defaultProps.zoom}
           >
 
-          </GoogleMapReact>
+          </GoogleMapReact> */}
+
+<iframe 
+src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60313.62604998036!2d72.86384894176219!3d19.125124710571857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c8081c1d7b43%3A0xbaf100c54b8be366!2sPowai%2C%20Mumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1718298864893!5m2!1sen!2sin" width="600" height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
 
