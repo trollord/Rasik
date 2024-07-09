8b5ff6d2649c2f91390fce7777825bb338ce3ddf
trollord <mkasangottuwar@clarapath.com>
2024-07-09 09:44:42 +0530

    centeterd

--- src/modules/commonComponents/header/index.desktop.tsx
+++ src/modules/commonComponents/header/index.desktop.tsx
@@ -183,7 +183,7 @@ export function DesktopHeader() {
             <Grid item md={3}>
                 <Typography sx={{ fontSize: '20px', color: '#737373' }}>Advertising Agency</Typography>
             </Grid>
-            <Grid item height="82%" onClick={() => navigateAndScroll('/', 'carousel')} md={2} display="flex" justifyContent="center" alignItems="center" sx={{ paddingLeft: "10px !important", cursor: "pointer", marginLeft: '10vw' }} >
+            <Grid item height="82%" onClick={() => navigateAndScroll('/', 'carousel')} md={2} display="flex" justifyContent="center" alignItems="center" sx={{ paddingLeft: "10px !important", cursor: "pointer" }} >
                 <img src="/assets/rasik_logo.svg" alt="logo" width="100%" height={"90%"} />
             </Grid>
             <Grid item display="flex" justifyContent="space-around" alignItems="center" md={3}   >
