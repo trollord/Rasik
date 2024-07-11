857ee8993d4d4fd354f7a2affe2e24db5126e844
Parth <parthdharmale008@gmail.com>
2024-07-11 21:40:12 +0530

    aboutUs changed and left aligned

--- src/modules/homePage/components/aboutUs/index.tsx
+++ src/modules/homePage/components/aboutUs/index.tsx
@@ -18,16 +18,16 @@ export function About() {
         {/* <Typography >Who are we?</Typography> */}
 
         {/* <Typography sx={{wordWrap:'break-word',textIndent:'50px'}}>We are amongst the select few accredited with INS. It empowers us to deal directly with media houses across India. We have an excellent rapport with all the leading publications all over India. Hence you get the best ever rates & deals for your ads releases. You name anything in the world of print, electronic and web advertising and the entire range of services come to you without compromising on quality and without an iota of doubt. Our experience and expertise ensure that you get maximum impact for every Rupee of your media expenditure.</Typography> */}
-        <Typography  textAlign={isMobile?"center":"center"} sx={{ fontSize: { xs: '15px', md: '20px' } }}>
+        <Typography  textAlign={isMobile?"left":"center"} sx={{ fontSize: { xs: '15px', md: '20px' } }}>
           <span 
             className={styles.weAreAmongst}
-          >{`At Rasik, we are one of the select few accredited with INS, allowing us to work directly with media houses across India. Our strong relationships with leading publications nationwide ensure the best rates and deals for your ad releases. From print to electronic and web advertising, we offer a comprehensive range of services without compromising on quality. With our extensive experience and expertise, we guarantee maximum impact for every rupee of your media expenditure.`}</span>
+          >{`Welcome to Rasik Publicity, India’s trusted INS-accredited advertising agency. With over six decades of experience, we specialize in comprehensive Print and digital marketing solutions, including Google Ads, social media strategies, E-commerce management, and Print releases—all at competitive rates. Our expertise spans both digital platforms and traditional print media, ensuring top-quality results and a powerful presence online and offline.`}</span>
           <span className={styles.blankLine}>&nbsp;</span>
           <span
             className={styles.withALegacy}
           >
             {/* `} <span style={{ textDecoration: 'underline', fontWeight: 'bold' }}>integrity, service, and commitment.</span>{` */}
-            {`With over six decades of accomplishments and progress, Rasik proudly upholds our founding principles of integrity, service, and commitment. By blending traditional wisdom with modern thinking and ethical conduct, we have earned an impeccable reputation among our clients and associates. This trust and support have helped us thrive, even during challenging times.  `}
+            {`We pride ourselves on delivering innovative advertising techniques tailored to meet your unique business needs. As one of the select few accredited with INS, we have direct access to media houses across India, ensuring you get the best rates and deals for your ad releases. Our strong relationships with leading publications nationwide guarantee maximum impact for every Rupee spent. Partner with us to enhance your visibility and engagement through quality advertising. `}
             </span>
           <span className={styles.blankLine1}>&nbsp;</span>
         </Typography>
