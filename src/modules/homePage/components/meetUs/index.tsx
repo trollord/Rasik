b238eebfe0e5098bb4585ad71f04d04e8fae20bc
Parth <parthdharmale008@gmail.com>
2024-07-22 23:01:15 +0530

    fixed few things acc to parth

--- src/modules/homePage/components/meetUs/index.tsx
+++ src/modules/homePage/components/meetUs/index.tsx
@@ -51,12 +51,12 @@ function MeetUs() {
             <Typography>109, Prime Plaza, J. V. Patel ITI Compound,Opp. Prabhadevi Railway Station, Prabhadevi Station (W)
               Mumbai- 400 013.</Typography>
           </Box>
-          <Box>
+          <Box width = "20vw">
             <Typography fontWeight="600" variant="h5">Email</Typography>
             {/* <Typography>109, Prime Plaza, J. V. Patel IT Compound,Opp. Prabhadevi Railway Station, Prabhadevi (W) 
                  Mumbai- 400 013.</Typography> */}
             <p className={styles.rasikads43gmailcomPrashant}>
-              <Grid className={styles.rasikEmail} height="4vh" width="40vw">
+              <Grid className={styles.rasikEmail} height="4vh" width="20vw">
 
 
                 <a
@@ -70,44 +70,48 @@ function MeetUs() {
 
                 </a>
               </Grid>
-              <Grid className={styles.prashantEmail}>
+              <Grid className={styles.prashantEmail}  width="20vw">
                 <a className={styles.rasikads43gmailcom}
                   href="mailto:prashant@rasikcommunications.com"
                   target="_blank"
                   >
-                    <Typography className={styles.prashantrasikcommunicationsc} fontSize="14px">
+                    {!isMobile && <Typography className={styles.prashantrasikcommunicationsc} fontSize="14px">
                       prashant@rasikcommunications.com
-                    </Typography>
+                    </Typography>}
+                    {isMobile && <Typography className={styles.prashantrasikcommunicationsc} fontSize="14px">
+                      prashant@rasikcommunications.
+                      com
+                    </Typography>}
                   
                 </a>
               </Grid>
             </p>
           </Box>
         </Grid>
-        <Grid container item md={3} justifyContent='left' flexDirection='column'>
-
-          <Typography fontWeight="600" variant="h5">Contacts</Typography>
-
-          <Box style={{ display: 'flex', flexDirection: 'column', gap: '10px', justifyContent: 'left' }}>
-            <Box>
+        <Grid container item md={3} justifyContent='left' flexDirection='column' >
+          {/* <Box height = "5vh">           */}
+            <Typography fontWeight="600" variant="h5" >Contacts</Typography>
+          {/* </Box> */}
+          <Box style={{ display: 'flex', flexDirection: 'column', gap: '10px', justifyContent: 'left' }}  >
+            <Box height="5vh" marginBottom="1vh">
               <Typography>Mr. Prashant Shah</Typography>
               <Typography> Phone : <a href="tel:+919768128256" style={{ color: 'black', textDecoration: 'none' }}>+91-9768128256</a></Typography>
             </Box>
-            <Box>
+            <Box height="5vh" marginBottom="1vh">
               <Typography>Mr. Parth Shah</Typography>
               
               <Typography> Phone : <a href="tel:+919821028256" style={{ color: 'black', textDecoration: 'none' }}>+91-9821028256</a></Typography>
               
             </Box>
-            <Box>
+            <Box height="5vh" marginBottom="1vh">
               <Typography>Mr. Rohit Naik</Typography> 
               <Typography>Phone : <a href="tel:+919768001616" style={{ color: 'black', textDecoration: 'none' }}>+91-9768001616</a></Typography>
             </Box>
-            <Box>
+            <Box height="5vh" marginBottom="1vh">
               <Typography>Mr. Vishal jadhav</Typography>
               <Typography>Phone : <a href="tel:+919773773434" style={{ color: 'black', textDecoration: 'none' }}>+91-9773773434</a></Typography>
             </Box>
-            <Box>
+            <Box height="5vh" marginBottom="1vh">
               <Typography>Ms. Twinkle Shah</Typography>
               <Typography>Phone : <a href="tel:+918898827588" style={{ color: 'black', textDecoration: 'none' }}>+91-8898827588</a></Typography>
             </Box>
